253ac6030cb2f5f37ac3ce82e4ead83c2775750f
Shun Usami <susami@c1r19s8.42tokyo.jp>
2022-04-09 21:01:00 +0900

    :sparkles: Add calloc test cases
    All returns non null ptr
    * size is 0
    * count is 0
    * size and count is 0

--- tests/ft_calloc_test.cpp
+++ tests/ft_calloc_test.cpp
@@ -16,11 +16,27 @@ int main(void)
 	signal(SIGSEGV, sigsegv);
 	title("ft_calloc\t: ")
 
-	void * p = ft_calloc(2, 2);
-	char e[] = {0, 0, 0, 0};
-	/* 1 */ check(!memcmp(p, e, 4));
-	/* 2 */ mcheck(p, 4); free(p); showLeaks();
+	void * p = ft_calloc(4, 8);
+	char e[] = {0, 0, 0, 0, 
+		0, 0, 0, 0, 
+		0, 0, 0, 0, 
+		0, 0, 0, 0, 
+		0, 0, 0, 0, 
+		0, 0, 0, 0, 
+		0, 0, 0, 0, 
+		0, 0, 0, 0};
+	/* 1 */ check(!memcmp(p, e, 32));
+	/* 2 */ mcheck(p, 32); free(p); showLeaks();
 	/* 3 */ check(ft_calloc(SIZE_MAX, SIZE_MAX) == NULL);
+	p = ft_calloc(0, 8);
+	/* 4 */ check(!memcmp(p, e, 0));
+	/* 5 */mcheck(p, 0); free(p); showLeaks();
+	p = ft_calloc(8, 0);
+	/* 6 */ check(!memcmp(p, e, 0));
+	/* 7 */ mcheck(p, 0); free(p); showLeaks();
+	p = ft_calloc(0, 0);
+	/* 8 */ check(!memcmp(p, e, 0));
+	/* 9 */ mcheck(p, 0); free(p); showLeaks();
 	write(1, "\n", 1);
 	return (0);
 }
