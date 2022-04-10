fa87dff1959eee0a29c0d0eb374d29b475523541
Shun Usami <susami@c1r19s8.42tokyo.jp>
2022-04-10 18:51:38 +0900

    :sparkles: Add isprint tests
    - min printable

--- tests/ft_isprint_test.cpp
+++ tests/ft_isprint_test.cpp
@@ -20,6 +20,16 @@ int main(void)
 	/* 2 */ check(ft_isprint(' ')); showLeaks();
 	/* 3 */ check(!ft_isprint('~' + 1)); showLeaks();
 	/* 4 */ check(ft_isprint('~')); showLeaks();
+	int	i = INT_MIN;
+	for (; i < INT_MAX && !ft_isprint(i); i++)
+		;
+	/* 5 */ check(i == 0x20); showLeaks();
+	for (; i < INT_MAX && ft_isprint(i); i++)
+		;
+	/* 6 */ check(i == 0x7f); showLeaks();
+	for (; i < INT_MAX && !ft_isprint(i); i++)
+		;
+	/* 7 */ check(i == INT_MAX); showLeaks();
 	write(1, "\n", 1);
 	return (0);
 }
